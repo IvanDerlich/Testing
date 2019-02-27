9feb6156fbcc8f3f31de01ba279ac431fd902bdf
Ivan Derlich <ivanderlich@gmail.com>
2019-02-27 15:29:51 -0300

    Primer commit

---
+++ assert.js
@@ -0,0 +1,11 @@
+function assert(actual, expected) {
+
+    console.log("Actual: " + actual);
+    console.log("Expected: " + expected);
+
+    if (actual === expected)
+        console.log("----APROVED----")
+    else
+        console.log("----DISAPROVED----");
+
+}
---
+++ tests.js
@@ -0,0 +1,4 @@
+assert(solution(1, 3), 4);
+assert(solution(1, 6), 7);
+assert(solution(1, 3), 3);
+assert(solution(1, 3), 9);
